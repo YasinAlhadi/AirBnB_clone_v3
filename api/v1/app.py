3cbad2068c30a8152e739dd1b4c8ca8550d918bf
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2023-02-14 20:20:55 +0200

    fix pycodestyle

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,9 +10,11 @@ app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
     storage.close()
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000", threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,7 @@
 from flask import jsonify
 from api.v1.views import app_views
 
+
 @app_views.route('/status')
 def status():
     """returns a JSON status"""
